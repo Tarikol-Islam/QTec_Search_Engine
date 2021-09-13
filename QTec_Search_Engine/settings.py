2296161deca980faa0fdee7c16b0b6fbb46419ca
Mohammad Tarikol Islam <tuhidtitw98@gmail.com>
2021-09-13 12:34:19 +0600

    Update settings.py

--- QTec_Search_Engine/settings.py
+++ QTec_Search_Engine/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'QTec_Search_Engine.wsgi.application'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
